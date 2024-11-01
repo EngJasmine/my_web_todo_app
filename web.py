57b82e1294d1903a8eaada9e6db0d968a028bdd0
Yasmine-Nasri <yasmine_nasri@yahoo.fr>
2024-11-01 16:50:48 -0400

    Set the page configuration

--- web.py
+++ web.py
@@ -1,9 +1,9 @@
 import streamlit as st
-
 import functions
 
 todos=functions.get_todos()
-
+#Method allowing to config the page
+st.set_page_config(layout='wide')
 def add_todo():
     to_do=st.session_state['new_todo']+ '\n'
     todos.append(to_do )
@@ -12,7 +12,8 @@ def add_todo():
 
 st.title("My TODO Application")
 st.subheader('This is my todo App.')
-st.write('This app is to increase your productivity.')
+st.write("This app is to increase your <b>productivity</b>.",
+         unsafe_allow_html=True)
 
 for index,todo in enumerate(todos):
     check_box=st.checkbox(todo,key=todo)
@@ -21,8 +22,5 @@ for index,todo in enumerate(todos):
         functions.write_todos(todos)
         del st.session_state[todo]
         st.rerun()
-
 st.text_input(label=' ',placeholder='Add new todo',
               on_change=add_todo,key='new_todo')
-st.session_state
-
